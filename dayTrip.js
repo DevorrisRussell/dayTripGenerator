1f44fe781770e8c74af981b130703b5b4945d0f1
Devorris Russell <devorris_r@yahoo.com>
2021-10-25 17:19:17 -0400

    adding loop to code to rerun options

--- dayTrip.js
+++ dayTrip.js
@@ -2,34 +2,41 @@
 
 // Need a destination to be randomly selected for trip
 
- 
-let destination = ["Tampa", "Orlando",
-"Miami", "Key West", "Daytona"];
-let myDestination = destination[Math.floor(Math.random() * 4)]; 
-   
+let notSatisfied = true;
+while(notSatisfied == true){
 
-let restaurant = ["Burns", "Shell",
-"Black Rock","Ulele","Cena"]; 
-let myRestaurant = restaurant[
-    Math.floor(Math.random() * 4)];
+
+    let destination = ["Tampa", "Orlando",
+    "Miami", "Key West", "Daytona"];
+    let myDestination = destination[Math.floor(Math.random() * 4)]; 
     
 
-let transportation = ["Car","Bike",
-"Scooter","Walk","Uber"]; let myTransportation = transportation[
-    Math.floor(Math.random() * 4)];
-   
-let entertainment = ["Dancing", "Concert", 
-"Art Gallery", "Beach", "Shopping"];
+    let restaurant = ["Burns", "Shell",
+    "Black Rock","Ulele","Cena"]; 
+    let myRestaurant = restaurant[
+        Math.floor(Math.random() * 4)];
+        
 
-let myEntertainment = entertainment[
-    Math.floor(Math.random() * 4)];
+    let transportation = ["Car","Bike",
+    "Scooter","Walk","Uber"]; let myTransportation = transportation[
+        Math.floor(Math.random() * 4)];
+    
+    let entertainment = ["Dancing", "Concert", 
+    "Art Gallery", "Beach", "Shopping"];
 
-alert(`${myDestination}\n${myRestaurant}\n
-${myTransportation}\n${myEntertainment}`)
+    let myEntertainment = entertainment[
+        Math.floor(Math.random() * 4)];
 
-let myTrip = confirm("Is this trip good?");
-console.log(myTrip); // Yes = true, No = False;
-if (myTrip){ // if true
-alert("Let's go Honey!");}
-else {// if false 
-prompt("What would you like to change?")
+    alert(`${myDestination}\n${myRestaurant}\n
+    ${myTransportation}\n${myEntertainment}`)
+
+    let myTrip = confirm("Is this trip good?");
+    console.log(myTrip); // Yes = true, No = False;
+    if (myTrip){ // if true
+    alert("Let's go Honey!");
+    notSatisfied = false;
+    }
+    else {// if false 
+    alert("Building whole new trip")
+    }
+}
